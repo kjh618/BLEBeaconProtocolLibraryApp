728d54c2bccb29cf6f94ac42fbacf1b527bdf88d
kjh <kimjihoon6188@daum.net>
2017-12-10 13:19:51 +0900

    Add ScanFilter

---
+++ BleBeaconProtocolLibrary/src/main/java/kr/hs/gshs/blebeaconprotocollibrary/ScanFilter.java
@@ -0,0 +1,21 @@
+package kr.hs.gshs.blebeaconprotocollibrary;
+
+/**
+ * Created by kjh on 2017-12-10.
+ */
+
+public class ScanFilter {
+    private boolean[] isBlocked = new boolean[PacketTypes.getValues().length];
+
+    public void block(PacketTypes packetType) {
+        isBlocked[packetType.ordinal()] = true;
+    }
+
+    public void unblock(PacketTypes packetType) {
+        isBlocked[packetType.ordinal()] = false;
+    }
+
+    public boolean isBlocked(PacketTypes packetType) {
+        return isBlocked[packetType.ordinal()];
+    }
+}
