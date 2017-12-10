4a8efc0b3cc6e973576ddd3ae2484eb6e1e31efc
kjh <kimjihoon6188@daum.net>
2017-12-10 14:51:46 +0900

    Actually fix possible crash with size 1 rawBytes

--- BleBeaconProtocolLibrary/src/main/java/kr/hs/gshs/blebeaconprotocollibrary/AdvertiseDataBuilder.java
+++ BleBeaconProtocolLibrary/src/main/java/kr/hs/gshs/blebeaconprotocollibrary/AdvertiseDataBuilder.java
@@ -39,15 +39,16 @@ public class AdvertiseDataBuilder {
         }
 
         int part1;
+        byte[] part2;
         if(rawBytes.size() <= 1) {
             part1 = rawBytes.get(0);
+            part2 = new byte[0];
         } else {
             part1 = 256 * rawBytes.get(1) + rawBytes.get(0);
-        }
-
-        byte[] part2 = new byte[rawBytes.size()-2];
-        for(int i=0; i<part2.length; ++i) {
-            part2[i] = rawBytes.get(i+2);
+            part2 = new byte[rawBytes.size()-2];
+            for(int i=0; i<part2.length; ++i) {
+                part2[i] = rawBytes.get(i+2);
+            }
         }
 
         AdvertiseData.Builder dataBuilder = new AdvertiseData.Builder();
